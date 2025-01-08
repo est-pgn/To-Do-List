0bc14788f0365534f11e34eb56a60f7c58612689
est-pgn <160342096+est-pgn@users.noreply.github.com>
2025-01-08 18:13:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById('add-task-btn').addEventListener('click',addTask);
+document.getElementById('task-input').addEventListener('keypress',function(event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+} );
+
+function addTask() {
+    const taskInput = document.getElementById('task-input');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') { 
+const tasklist = document.getElementById('task-list');
+const taskItem = document.createElement('li');
+
+const taskSpan = document.createElement('span');
+taskSpan.textContent = taskText;
+taskSpan.addEventListener('click', function() { 
+taskSpan.parentNode.classList.toggle('completed');
+ });
+
+const deleteButton = document.createElement('button');
+deleteButton.textContent = 'Delete';
+deleteButton.addEventListener('click', function() { 
+tasklist.removeChild(taskItem);
+ });
+
+taskItem.appendChild(taskSpan);
+taskItem.appendChild(deleteButton);
+tasklist.appendChild(taskItem);
+taskInput.value = '' ;
+}
+ }
